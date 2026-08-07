ac937c68ed1f2abbf4da8d90d52db32febdfab8e
Muppu Kokila <127586949+Muppukokila@users.noreply.github.com>
2026-08-07 12:53:18 +0530

    Update subject ID references in enrollment logic

--- src/components/dialog_enroll.py
+++ src/components/dialog_enroll.py
@@ -12,24 +12,17 @@ def enroll_dialog():
 
     if st.button('Enroll now', type='primary', width='stretch'):
         if join_code:
-            res = supabase.table('subjects').select('id, name, subject_code').eq('subject_code', join_code).execute()
+            res = supabase.table('subjects').select('subject_id, name, subject_code').eq('subject_code', join_code).execute()
             if res.data:
                 subject = res.data[0]
-                student_id = st.session_state.student_data['id']
-                st.write("Session Student:", st.session_state.student_data)
+                student_id = st.session_state.student_data['student_id']
 
-                check = supabase.table('subject_students').select('*').eq('subject_id', subject['id']).eq('student_id', student_id).execute()
+                check = supabase.table('subject_students').select('*').eq('subject_id', subject['subject_id']).eq('student_id', student_id).execute()
                 if check.data:
                     st.warning('You are already enrolled in this program')
                 else:
-                    st.write("Student ID:", student_id)
-                    st.write("Subject ID:", subject["id"])
-
-                    response = enroll_student_to_subject(student_id, subject["id"])
-
-                    st.write("Insert Response:", response)
-
-                    st.success("Successfully enrolled!")
+                    enroll_student_to_subject(student_id, subject['subject_id'])
+                    st.success('Succesfully enrolled!')
                     time.sleep(1)
                     st.rerun()
         else:
